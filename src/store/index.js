aa90c20467f243a4350e66a7f8f8504cded33b9b
kitty.cheng <450038893@qq.com>
2020-10-04 23:21:07 +0800

    vuex and validator

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import promiseMiddleware from 'redux-promise'
+// import { createStore, applyMiddleware } from 'redux';
+// import promiseMiddleware from 'redux-promise'
 
-import rootReducer from './reducers';
+// import rootReducer from './reducers';
 
-export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
+// export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
+
+import Vuex from '@wepy/x'
+import users from './modules/user'
+
+export default new Vuex.Store({
+    modules: {
+        users,
+    }
+})
